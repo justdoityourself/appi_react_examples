1c0a107be011d85f7c323e5c87ec4a7aec74c74a
Andrew Gardner <41390536+justdoityourself@users.noreply.github.com>
2022-05-25 07:51:31 -0600

    Show code for example 4

--- src/examples/Example4.jsx
+++ src/examples/Example4.jsx
@@ -35,4 +35,37 @@ export default function Example() {
 
 export const subtitle = "Push / Commit Local Changes";
 
-export const code = `Pending Finalization`;
+export const code = `import {useState} from 'react';
+import {loadBasic} from 'appi_react'
+
+export default function Example() {
+    const [result,setResult] = useState("");
+
+    const getDirty = async ()=>{
+
+        const client = await loadBasic({});
+
+        setResult(client.Dirty());
+    };
+
+    const sync = async ()=>{
+
+        const client = await loadBasic({});
+
+        await client.Commit();
+
+        getDirty();
+    };
+
+    return ( 
+        <div>
+            <button onClick={()=>getDirty()}>
+                Load Dirty
+            </button>
+            <button onClick={()=>sync()}>
+                Commit
+            </button>
+            <textarea readOnly style={{margin:8,width:'100%'}} rows="16" value={result} />
+        </div> 
+    );
+}`;
